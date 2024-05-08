9799e76fc4cab420c770c1afa9bc01f7b58f96d0
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-07 18:27:20 -0700

    wraps module section (nb 'pub use')

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -9,10 +9,10 @@
 
 // I AM NOT DONE
 
-mod delicious_snacks {
+pub mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
