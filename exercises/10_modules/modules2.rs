7d9e2e0d5aec9fbb5caa6feb58bf3184f0770729
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-07 19:28:02 -0700

    fixes small checker bug

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub mod delicious_snacks {
     // TODO: Fix these use statements
     pub use self::fruits::PEAR as fruit;
