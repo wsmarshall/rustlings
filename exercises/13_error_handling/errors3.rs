a489f5a7e62a8692e3ea4d27241154a89b03d357
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-05-13 17:25:56 -0700

    changes main to return Result for errors3

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -7,25 +7,21 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = match total_cost(pretend_user_input) {
-        Ok(num) => {
-            if num > tokens {
-                println!("You can't afford that many!");
-            } else {
-                tokens -= num;
-                println!("You now have {} tokens.", tokens);
-            }
-        }
-        Err(e) => panic!("Error: {:?}", e),
-    };
+    let cost = total_cost(pretend_user_input)?;
+
+    if cost > tokens {
+        println!("You can't afford that many!");
+    } else {
+        tokens -= cost;
+        println!("You now have {} tokens.", tokens);
+    }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
