782b2a36068b5439e3384da53b428a51612e5afe
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-06-28 06:34:49 -0700

    reverts to a slightly earlier vers

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -33,10 +33,11 @@ impl ParsePosNonzeroError {
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    match s.parse() {
-        Ok(Some(x)) => PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation),
-        Err(Some(x)) => ParsePosNonzeroError::from_parseint,
-    }
+    let x: i64 = s
+        .parse()
+        .unwrap()
+        .map_err(ParsePosNonzeroError::from_parseint);
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
 // Don't change anything below this line.
