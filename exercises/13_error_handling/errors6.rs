f8d2bb9e661068cd46cb17e26de8019a509f0c35
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-06-28 01:09:05 -0700

    cont error6 - match arms not matching

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -33,11 +33,10 @@ impl ParsePosNonzeroError {
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s
-        .parse()
-        .unwrap()
-        .map_err(ParsePosNonzeroError::from_parseint);
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    match s.parse() {
+        Ok(Some(x)) => PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation),
+        Err(Some(x)) => ParsePosNonzeroError::from_parseint,
+    }
 }
 
 // Don't change anything below this line.
