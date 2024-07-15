af0e9e3358003ef448ac8bb33efcfbe6fe849301
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-07-15 14:47:38 -0700

    starts quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -20,14 +20,17 @@
 
 pub struct ReportCard {
     pub grade: f32,
+    pub grade: String,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -52,7 +55,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
