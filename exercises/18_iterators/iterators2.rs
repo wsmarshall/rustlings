6b6dbd9ce5132e2e8123635d2288da7d693de5cf
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-07-25 14:50:19 -0700

    continues iterators2

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -15,9 +15,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.to_string().to_uppercase(),
+        Some(first) => {
+            first.make_ascii_uppercase();
+            c.to_string()
+        }
     };
-    c.to_string()
 }
 
 // Step 2.
