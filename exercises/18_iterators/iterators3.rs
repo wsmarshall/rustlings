54c8dd500b9a900e8b3272a6b2dcaa2fdf4898fb
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-07-29 13:16:41 -0700

    makes all tests pass up to (n/i) test_result_with_list

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -26,6 +26,10 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+
     match a % b {
         0 => Ok(a / b),
         _ => Err(DivisionError::NotDivisible(NotDivisibleError {
@@ -60,31 +64,31 @@ mod tests {
         assert_eq!(divide(81, 9), Ok(9));
     }
 
-    // #[test]
-    // fn test_not_divisible() {
-    //     assert_eq!(
-    //         divide(81, 6),
-    //         Err(DivisionError::NotDivisible(NotDivisibleError {
-    //             dividend: 81,
-    //             divisor: 6
-    //         }))
-    //     );
-    // }
+    #[test]
+    fn test_not_divisible() {
+        assert_eq!(
+            divide(81, 6),
+            Err(DivisionError::NotDivisible(NotDivisibleError {
+                dividend: 81,
+                divisor: 6
+            }))
+        );
+    }
 
-    // #[test]
-    // fn test_divide_by_0() {
-    //     assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
-    // }
+    #[test]
+    fn test_divide_by_0() {
+        assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
+    }
 
-    // #[test]
-    // fn test_divide_0_by_something() {
-    //     assert_eq!(divide(0, 81), Ok(0));
-    // }
+    #[test]
+    fn test_divide_0_by_something() {
+        assert_eq!(divide(0, 81), Ok(0));
+    }
 
-    // #[test]
-    // fn test_result_with_list() {
-    //     assert_eq!(format!("{:?}", result_with_list()), "Ok([1, 11, 1426, 3])");
-    // }
+    #[test]
+    fn test_result_with_list() {
+        assert_eq!(format!("{:?}", result_with_list()), "Ok([1, 11, 1426, 3])");
+    }
 
     // #[test]
     // fn test_list_of_results() {
