f45daac88ce99a13a6df204a93028f6d5a4d1539
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-07-30 18:33:39 -0700

    impls iterators4 (all tests pass, meets constraints)

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,13 +13,8 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
-    match num {
-        0 => 0,
-        _ => {
-            let list: Vec<u64> = (1..num).collect();
-            let mut iter = list.iter();
-        }
-    }
+
+    (1..num + 1).into_iter().fold(1, |acc, n| acc * n)
 }
 
 #[cfg(test)]
