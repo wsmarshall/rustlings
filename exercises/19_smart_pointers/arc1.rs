b7ca792813f23472aff9157fe478efdab4873db8
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-08-06 18:00:19 -0700

    starts arc1.rs

--- exercises/19_smart_pointers/arc1.rs
+++ exercises/19_smart_pointers/arc1.rs
@@ -29,7 +29,7 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers // TODO
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
